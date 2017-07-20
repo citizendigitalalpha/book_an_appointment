35416d3c38cb9f83f44ab2a2aa7183c3dc0d3bb6
Ashley Anderton <ashley.anderton@civil.lmco.com>
2017-07-20 16:08:15 +0100

    More changes on appointments page

--- app/assets/js/custom.js
+++ app/assets/js/custom.js
@@ -1,14 +1,19 @@
-//Test
-
 $(function () {  
     $(".appointmentbutton").click(function () {
         $(".appointment-first").addClass("hidden");
         $(".appointment-times").removeClass("hidden");
     });
-});
 
-//$(function () {                       
-//    $(".appointmentbutton").click(function () {  
-//        $(this).addClass("active");     
-//    });
-//});
+    $(".locationbutton").click(function () {
+        $(".locationchoose").addClass("hidden");
+        $(".location-box").removeClass("hidden");
+    });
+
+
+    $(".locationsubmit").click(function () {
+        var location = $('#customer-location').val();
+        $(".locationplace").text(location);
+        $(".locationchoose").removeClass("hidden");
+        $(".location-box").addClass("hidden");
+    });
+});
